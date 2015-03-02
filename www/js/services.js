2e42e0793573bfef19dff2ddcb9c4de689bdabf6
danieltajamar <danielmfdez@gmail.com>
2015-03-02 19:05:00 +0100

    bbdd

--- www/js/services.js
+++ www/js/services.js
@@ -1,96 +1,107 @@
-angular.module('starter.services', [])
+//var fecha = new Date();
+//console.log(mensaje + " ejecutado el " + fecha.getDate() + " del " + fecha.getMonth()  + " de " + fecha.getFullYear());
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+//angular.module('starter.services', [])
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Andrew Jostlin',
-    lastText: 'Did you get the ice cream?',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
-  }];
+  /*  .factory('Bbdd',function($q){
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+        var busqueda;
+
+        //Funcion para crear la base de datos
+        var db = openDatabase("Bbdd", "", "Base Tareas", 1024 * 1024,
+            function (db) {
+                db.transaction(function (tx) {
+                        tx.executeSql(
+                            "create table if not exists Tareas " +
+                            "(Id integer primary key autoincrement, " +
+                            "Nombre text, " +
+                            "Descripcion text," +
+                            "Fecha date," +
+                            "Prioridad text," +
+                            "Estado text)" );
+                    },
+
+                    function (err) {
+
+                        alert(err.toString());
+
+                    });
+            });
+
+        return{
+            setBusqueda:function(bus){
+
+                busqueda=bus;
+            },
+            getBusqueda:function(){
+                return busqueda;
+
+            },
+
+
+            addTarea:function(producto){
+
+                var db = openDatabase("Bbdd", "", "Base Tareas", 1024 * 1024);
+
+                db.transaction(function (tx) {
+
+                    tx.executeSql("insert into Tarea"+
+                        " (Nombre,Precio) values(?,?)",
+                        [producto.nombre, producto.precio]
+                    );
+                });
+
+            },
+            buscarProducto:function(nombre){
+                var db = openDatabase("Productos", "", "Base productos",
+                    1024 * 1024);
+
+                var deferred=$q.defer();
+
+                db.transaction(function(tx){
+
+                    tx.executeSql("select * from Producto where nombre"+
+                        " like ?",["%"+nombre+"%"],
+
+                        function(tran,res){
+
+                            var productos=[];
+
+                            for(var i=0;i<res.rows.length;i++){
+                                var o={
+                                    id:res.rows.item(i).id,
+                                    nombre:res.rows.item(i).nombre,
+                                    precio:res.rows.item(i).precio
+
+                                };
+                                productos.push(o);
+
+                            }
+
+
+                            deferred.resolve(productos);
+
+                        },
+                        function(tran,err){
+                            deferred.reject(err);
+
+                        }
+                    );
+
+
+
+                });
+                return deferred.promise;
+
+
+
+            }
+
         }
-      }
-      return null;
-    }
-  }
-})
-
-/**
- * A simple example service that returns some data.
- */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var friends = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    notes: 'Enjoys drawing things',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    notes: 'Odd obsession with everything',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Andrew Jostlen',
-    notes: 'Wears a sweet leather Jacket. I\'m a bit jealous',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    notes: 'I think he needs to buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    notes: 'Just the nicest guy',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
-  }];
 
 
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
-    }
-  }
-});
+
+
+
+    });
+*/
