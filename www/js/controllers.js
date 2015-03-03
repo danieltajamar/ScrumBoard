e89d01c33d536cfb9b600c24ecb92e23dad86c2d
danieltajamar <danielmfdez@gmail.com>
2015-03-03 18:53:44 +0100

    factory

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,175 @@
 angular.module('starter.controllers', [])
 
+/*    .controller('LoginCtrl', function($scope,$ionicLoading,$ionicPopup,$state,$ionicPlatform,Usuarios,Conexion) {
+        $scope.proyecto={};
+
+        $scope.iniciarSesion=function(){
+            $ionicLoading.show(
+                {
+                    template:'Validando Proyecto'
+
+                });
+
+            Proyectos.validarProyecto($scope.proyecto).then(
+                function(res){
+                    $ionicLoading.hide();
+                    if(res.length>0) {
+                        localStorage.proyecto = JSON.stringify(res[0]);
+                      //  $state.go("tab.blocs");
+                    }
+                    else{
+                        $ionicPopup.alert({
+                            template:'Credenciales incorrectas',
+                            title: '¡Error!'
+                        });
+                    }
+                },
+                function(err){
+                    $ionicLoading.hide();
+                    $ionicPopup.alert({
+                        template:'Error al validar el usuario',
+                        title: '¡Error!'
+                    });
+                });
+        };
+
+
+        $ionicPlatform.ready(function(){
+            if(localStorage.proyecto){
+                $scope.proyecto=JSON.parse(localStorage.proyecto);
+                if(Conexion.getEstado()){
+                    $scope.iniciarSesion();
+                }
+
+                else
+                {
+             //       $state.go("tab.blocs");
+
+                }
+
+            }
+
+        });
+
+    })
+
+    .controller('RegistroCtrl', function($scope,$http,$state,$ionicLoading,$ionicPopup) {
+        $scope.proyecto={};
+
+        $scope.registro=function(){
+            var url="https://proyectoscrumboard.azure-mobile.net/Proyectos";
+            $http.defaults.headers.common={
+                'X-ZUMO-APPLICATION':'BRhCygIesoMFkJCuwIeuHuVLljRDWO94',
+                'Access-Control-Allow-Origin':'*'
+
+            };
+
+            $ionicLoading.show(
+                {
+                    template:'Creando Proyecto'
+
+                }
+
+            );
+
+            $http.post(url,$scope.proyecto).then(
+                function(res){
+                    $ionicLoading.hide();
+
+                    $ionicPopup.alert({
+                        template:'Proyecto creado con exito',
+                        title: '¡Exito!'
+
+                    });
+
+                 //   $state.go("noLogin.login");
+
+                }
+                ,
+                function(err){
+                    $ionicLoading.hide();
+
+                    $ionicPopup.alert({
+                        template:'Error al creando el proyecto',
+                        title: '¡Error!'
+
+                    });
+
+                }
+
+            );
+
+        }
+
+    })
+
+    .controller('BlocsCtrl', function($scope,Tareas,Bbdd,Conexion) {
+        $scope.Tareas=[];
+
+        var pr=JSON.parse(localStorage.Proyecto);
+
+        if(Conexion.getEstado()){
+            Tareas.getTareasPorProyecto(pr.Nombre).then(function(res){
+
+                    $scope.Tareas=res;
+                    Bbdd.guardarBlocs(res);
+
+
+                },
+                function(err){
+                    alert(err);
+
+                });
+
+        }
+        else{
+
+            Bbdd.().then(function(res){
+                $scope.blocs=res;
+
+
+            },function(err){
+                alert(err);
+
+            });
+
+        }
+
+    })
+
+    .controller('BlocDetailCtrl', function($scope, $stateParams,Conexion, Tareas,Bbdd) {
+
+        $scope.tareas=[];
+
+        if(Conexion.getEstado()) {
+            Tareas.getTareasPorProyecto($stateParams.proyecto).then(
+                function (res) {
+                    $scope.tareas = res;
+                    Bbdd.guardarTareas(res);
+
+                },
+                function (err) {
+                    alert(err);
+                }
+            );
+
+        }
+        else{
+            Bbdd.buscarBacklog($stateParams.blocId).then(
+                function (res) {
+                    $scope.tareas=res;
+                },
+                function(err){
+
+                    alert(err);
+                }
+
+            );
+
+        }
+
+    })
+*/
 .controller('DashCtrl', function($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
--- www/js/services.js
+++ www/js/services.js
@@ -49,9 +49,6 @@ angular.module('starter.services', [])
         }
     })
 
-/**
- * A simple example service that returns some data.
- */
     .factory('Friends', function() {
         // Might use a resource here that returns a JSON array
 
@@ -93,13 +90,107 @@ angular.module('starter.services', [])
                 return friends[friendId];
             }
         }
-    });
+    })
 
 // Backlog=1, To-do=2, In review=3, Done=4
 // Baja=1, Media=2, Alta=3
-/*
-angular.module('starter.services', [])
 
+    .factory('Proyectos', function ($http, $q) {
+        var url = "https://proyectoscrumboard.azure-mobile.net/Proyectos";
+
+        $http.defaults.headers.common = {
+            'X-ZUMO-APPLICATION': 'BRhCygIesoMFkJCuwIeuHuVLljRDWO94',
+            'Access-Control-Allow-Origin': '*'
+
+        };
+
+        return {
+            validarProyecto: function (Proyecto) {
+                var query = "?$filter=Proyecto eq '" + Proyecto.Nombre +
+                    "' and Contraseña eq '" + Proyecto.Contraseña + "'";
+                var request = $http(
+                    {
+                        url: url + query,
+                        method: 'get'
+
+                    });
+
+                return request.then(ok, err);
+            }
+        };
+
+        function ok(resp) {
+            return resp.data;
+
+        }
+
+        function err(resp) {
+            if (!angular.isObject(resp.data) || !resp.data.message) {
+                return ($q.reject("Error desconocido"));
+
+            }
+            return ($q.reject(resp.data.message));
+        }
+
+    })
+
+    .factory('Tareas', function ($http, $q) {
+        var url = "https://proyectoscrumboard.azure-mobile.net/Tareas";
+        $http.defaults.headers.common = {
+            'X-ZUMO-APPLICATION': 'BRhCygIesoMFkJCuwIeuHuVLljRDWO94',
+            'Access-Control-Allow-Origin': '*'
+
+        };
+
+        return {
+            getTareasPorProyecto: function (Proyecto) {
+                var query = "?$filter=Proyecto eq '" + Proyecto + "'";
+                var request = $http(
+                    {
+                        url: url + query,
+                        method: 'get'
+
+                    });
+
+                return request.then(ok, err);
+
+            }
+
+        };
+
+        function ok(resp) {
+            return resp.data;
+
+        }
+
+        function err(resp) {
+            if (!angular.isObject(resp.data) || !resp.data.message) {
+                return ($q.reject("Error desconocido"));
+
+            }
+            return ($q.reject(resp.data.message));
+        }
+    })
+    .factory('Conexion', function () {
+
+        return {
+            getEstado: function () {
+
+                try {
+                     var conn = navigator.connection.type;
+
+                     if (conn == Connection.NONE || conn == Connection.UNKNOWN ||
+                     conn == Connection.CELL)
+                     return false;
+
+                } catch (e) {
+
+                    alert(e.toString());
+                }
+            }
+
+        }
+    })
     .factory('Bbdd',function($q){
 
         var busqueda;
@@ -111,6 +202,10 @@ angular.module('starter.services', [])
             function (db) {
 
                 db.transaction(function (tx) {
+
+                        tx.executeSql("create table if not exists Proyectos" +
+                        "(Nombre text, Contraseña text)");
+
                         tx.executeSql(
                             "create table if not exists Tareas " +
                             "(Id integer primary key autoincrement, " +
@@ -118,7 +213,8 @@ angular.module('starter.services', [])
                             "Descripcion text," +
                             "Fecha date," +
                             "Prioridad integer," +
-                            "Estado integer)" );
+                            "Estado integer" +
+                            "Proyecto text)" );
                     },
 
                     function (err) {
@@ -129,6 +225,7 @@ angular.module('starter.services', [])
             });
 
         return{
+
             setBusqueda:function(bus){
 
                 busqueda=bus;
@@ -138,28 +235,45 @@ angular.module('starter.services', [])
 
             },
 
+            guardarProyecto: function (Proyecto) {
+                var db = openDatabase("Bbdd", "", "Base Tareas", 1024 * 1024);
 
-            addTarea:function(tarea){
+                db.transaction(function (tx) {
+                    tx.executeSql("delete from Proyectos");
+
+                    for (var i = 0; i < Proyecto.length; i++) {
+
+                        tx.executeSql("insert into Proyectos values(?,?)",
+                            [Proyecto[i].Nombre, Proyecto[i].Contraseña]
+                        );
+
+                    }
+                });
+            },
+
+            guardarTarea:function(Tarea, Proyecto){
 
                 var db = openDatabase("Bbdd", "", "Base Tareas", 1024 * 1024);
 
                 db.transaction(function (tx) {
+                    tx.executeSql("delete from Tareas where Proyecto=?",[Proyecto]);
 
-                    tx.executeSql("insert into Tarea"+
-                        " (Nombre,Descripcion, Fecha, Prioridad, Estado) values(?,?,?,?,?)",
-                        [tarea.nombre, tarea.Descripcion, fecha, tarea.Prioridad, tarea.Estado]
-                    );
+                        for (var i = 0; i < notas.length; i++) {
+                            tx.executeSql("insert into Tarea (Nombre,Descripcion, Fecha, Prioridad, Estado, Proyecto) values(?,?,?,?,?,?)",
+                                [Tarea.Nombre, Tarea.Descripcion, fecha, Tarea.Prioridad, Tarea.Estado, Tarea.Proyecto]
+                            );
+                        }
                 });
-
             },
-            buscarBacklog:function(Estado){
+
+            buscarTareas:function(Proyecto, Estado){
                 var db = openDatabase("Bbdd", "", "Base Tareas",1024 * 1024);
 
                 var deferred=$q.defer();
 
                 db.transaction(function(tx){
 
-                    tx.executeSql("select * from Tarea where Estado=?",[1],
+                    tx.executeSql("select * from Tareas where Estado=?",[Estado]," and Proyecto=?",[Proyecto],
 
                         function(tran,res){
 
@@ -167,18 +281,17 @@ angular.module('starter.services', [])
 
                             for(var i=0;i<res.rows.length;i++){
                                 var o={
-                                    id:res.rows.item(i).id,
-                                    nombre:res.rows.item(i).nombre,
+                                    Id:res.rows.item(i).id,
+                                    Nombre:res.rows.item(i).nombre,
                                     Descripcion:res.rows.item(i).Descripcion,
                                     Fecha:res.rows.item(i).Fecha,
                                     Prioridad:res.rows.item(i).Prioridad,
-                                    Estado:res.rows.item(i).Estado
-
+                                    Estado:res.rows.item(i).Estado,
+                                    Proyecto:res.rows.item(i).Proyecto
                                 };
                                 tareas.push(o);
 
                             }
-
 
                             deferred.resolve(tareas);
 
@@ -189,20 +302,10 @@ angular.module('starter.services', [])
                         }
                     );
 
-
-
                 });
                 return deferred.promise;
-
-
 
             }
 
         }
-
-
-
-
-
     });
-*/
