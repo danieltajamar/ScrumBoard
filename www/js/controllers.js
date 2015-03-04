cd31a3ab85787ff61dd756149c45b4eaec8541fc
JesusMarsan <jesus.martin@tajamar365.com>
2015-03-04 13:12:56 +0100

    modifico controller de modal proyecto

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -103,6 +103,34 @@ angular.module('starter.controllers', [])
         }
 
     })
+//Código para llamar al modal de proyecto
+angular.module('testApp', ['ionic'])
+    .controller('ProyectoCtrl', function($scope, $ionicModal) {
+        $ionicModal.fromTemplateUrl('modal-proyecto.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.modal = modal;
+        });
+        $scope.openModal = function() {
+            $scope.modal.show();
+        };
+        $scope.closeModal = function() {
+            $scope.modal.hide();
+        };
+        //Cleanup the modal when we're done with it!
+        $scope.$on('$destroy', function() {
+            $scope.modal.remove();
+        });
+        // Execute action on hide modal
+        $scope.$on('modal.hidden', function() {
+            // Execute action
+        });
+        // Execute action on remove modal
+        $scope.$on('modal.removed', function() {
+            // Execute action
+        });
+    })
 /*
     .controller('BlocsCtrl', function($scope,Tareas,Bbdd,Conexion) {
         $scope.Tareas=[];
